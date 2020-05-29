564f3f791d2ab943c4373b9e9147daef18da7ae9
kenorb <kenorb@users.noreply.github.com>
2020-05-29 20:46:53 +0100

    Adds traps

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,38 @@ set -o pipefail
 # Treat unset variables as an error and exit immediately.
 set -u
 
+# Defines default variables.
+verbose=0
+
+# Implements on-exit trap function.
+on_exit() {
+  # @param $1 integer (optional) Exit status. If not set, use '$?'.
+  local exit_status=${1:-$?}
+  [ "$verbose" -eq 1 ] && echo "[INFO] $0 finishes with exit code $exit_status."
+  exit "$exit_status"
+}
+# Implements on-error trap function.
+on_error() {
+  # @param $1 integer (optional) Exit status. If not set, use '$?'.
+  local exit_status=${1:-$?}
+  [ "$verbose" -eq 1 ] && echo "[ERROR] $0 finishes with exit code $exit_status!"
+  get_backtrace >&2
+  exit "$exit_status"
+}
+# Displays simple stack trace.
+get_backtrace() {
+  while caller $((n++)); do :; done
+}
+
+# Handle bash errors. Exit on error. Trap exit.
+# Trap normal exit signal (exit on all errors).
+trap on_exit EXIT
+
+# Trap non-normal exit signals: 1/HUP, 2/INT, 3/QUIT, 15/TERM, ERR.
+trap on_error 1 2 3 15 ERR
+
+# Defines variables.
+verbose="$( ! [ "${INPUT_LOGLEVEL:-0}" -gt 0 ]; echo $? )"
 args=()
 if [ -n "$INPUT_INPUTFILE" ]; then
   args+=( "$(printf -- "-i%s" "$INPUT_INPUTFILE")" )
