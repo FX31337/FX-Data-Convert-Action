ed072b518d5a474981baf2f5834ec64e000b0b8e
kenorb <kenorb@users.noreply.github.com>
2021-04-09 09:33:38 +0100

    Python string fixes

--- src/bstruct/bstruct.py
+++ src/bstruct/bstruct.py
@@ -6,7 +6,7 @@ import binascii
 
 def get_fields_size(spec):
     # Prepend an endianness mark to prevent calcsize to insert padding bytes
-    fmt_str = "=" + "".join(x[1] for x in spec)
+    fmt_str = '=' + ''.join(x[1] for x in spec)
     return struct.calcsize(fmt_str)
 
 
@@ -25,7 +25,7 @@ class BStruct:
             offset += field_size
 
     def __str__(self):
-        ret = ""
+        ret = ''
 
         for (name, _, *fmt) in self._fields:
             val_repr = getattr(self, name)
@@ -34,7 +34,7 @@ class BStruct:
                 (pp,) = fmt
                 val_repr = pp(self, getattr(self, name))
 
-            ret += "{} = {}\n".format(name, val_repr)
+            ret += '{} = {}\n'.format(name, val_repr)
 
         return ret
 
@@ -42,15 +42,15 @@ class BStruct:
         blob_size = get_fields_size(self._fields)
 
         if blob_size == 0:
-            return b""
+            return b''
 
         offset = 0
-        blob = bytearray(b"\x00" * blob_size)
+        blob = bytearray(b'\x00' * blob_size)
         for (name, fmt, *_) in self._fields:
             field_size = struct.calcsize(fmt)
             v = getattr(self, name)
 
-            if fmt[-1] == "s" or len(fmt) == 1:
+            if fmt[-1] == 's' or len(fmt) == 1:
                 v = [v]
 
             struct.pack_into(self._endianness + fmt, blob, offset, *v)
@@ -67,11 +67,11 @@ def pretty_print_time(obj, x):
 
 
 def pretty_print_string(obj, x):
-    return x.decode("utf-8").rstrip("\0")
+    return x.decode('utf-8').rstrip('\0')
 
 
 def pretty_print_wstring(obj, x):
-    return x.decode("utf-16").rstrip("\0")
+    return x.decode('utf-16').rstrip('\0')
 
 
 def pretty_print_bstring(obj, x):
@@ -79,18 +79,18 @@ def pretty_print_bstring(obj, x):
 
 
 def pretty_print_ignore(obj, x):
-    return "<...>"
+    return '<...>'
 
 
 def pretty_print_hex(obj, x):
-    return "{:08x}".format(x)
+    return '{:08x}'.format(x)
 
 
 def pretty_print_compact(obj, x):
     if any(x):
         return x
-    return "[\\x00] * {}".format(len(x))
+    return '[\\x00] * {}'.format(len(x))
 
 
 def pretty_print_decimal_p5(obj, x):
-    return "{0:.5f}".format(x)
+    return '{0:.5f}'.format(x)
